101c8fd29c115efb0416e36851f279c5f53d0393
JasonGuoSkr <gl18253166772@163.com>
2020-12-10 13:30:05 +0800

    update

--- code_v1/hpFilter.py
+++ code_v1/hpFilter.py
@@ -26,3 +26,19 @@ def hp(y, lamb=10):
     D = D2 @ D1
     g = np.linalg.inv((np.eye(N)+lamb*D.T@D))@ ts
     return g
+
+
+if __name__ == '__main__':
+    N = 100
+    t = np.linspace(1,10,N)
+    ts = np.sin(t) + np.cos(20*t) + np.random.randn(N)*0.1
+
+    plt.figure(figsize=(10,12))
+    for i,l in enumerate([0.1,1,10,100,1000, 10000]):
+        plt.subplot(3,2,i+1)
+        g = hp(ts,l)
+        plt.plot(ts, label='original')
+        plt.plot(g, label='filtered')
+        plt.legend()
+        plt.title('$\lambda$='+str(l))
+    plt.show()
